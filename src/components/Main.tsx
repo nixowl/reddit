38e843d398915404ab188b9d990461800a3d1b77
nixowl <gabriellamondvay@gmail.com>
2024-04-14 16:45:01 +0200

    add skeleton for loading state

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,10 @@
-import { useEffect } from 'react'
+import { Key, useEffect } from 'react'
 import { Feed } from './Feed'
 import { AppDispatch, RootState } from '@/redux'
 import { fetchPosts } from '@/redux/postsSlice'
 import { useAppSelector, useAppDispatch } from '@/hooks'
 import { setSortingOption } from '@/redux/sortingSlice'
+import { SkeletonCard } from './ui/skeletonCard'
 
 export const Main = () => {
     const dispatch: AppDispatch = useAppDispatch()
@@ -24,9 +25,18 @@ export const Main = () => {
     return (
         <div>
             {isLoading ? (
-                <p>Loading...</p>
+                <>
+                    {Array.from({ length: 5 }).map(
+                        (_: any, index: Key | null | undefined) => (
+                            <SkeletonCard key={index} />
+                        )
+                    )}
+                </>
             ) : error ? (
-                <p>{error}</p>
+                <>
+                    <h2 className="text-3xl">Something went wrong 😱</h2>
+                    <p>{error}</p>
+                </>
             ) : (
                 <Feed postsArray={posts} className="max-w-screen-lg" />
             )}
