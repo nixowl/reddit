bbbd95eb4a7e35c093397b4ad6e484645b433aa6
nixowl <gabriellamondvay@gmail.com>
2024-04-15 12:42:03 +0200

    fix skeleton styling to better match feed content cards

--- src/components/ui/skeletonCard.tsx
+++ src/components/ui/skeletonCard.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from '@/components/ui/skeleton'
 
 export function SkeletonCard() {
     return (
-        <div className="py-2 px-11 flex flex-col space-y-3 max-w-screen-lg">
+        <div className="py-2 px-4 flex flex-col space-y-3 max-w-screen-xl">
             <Skeleton className="h-[300px] rounded-xl" />
             <div className="space-y-2 p-4">
                 <Skeleton className="w-[250px]" />
