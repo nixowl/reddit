ce2fdad9720a8384b21c53a651d62d2471ba9b49
nixowl <gabriellamondvay@gmail.com>
2024-04-15 12:38:36 +0200

    add routing logic which renders feed content based on the current state of sortingOption

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -4,8 +4,9 @@ import { Card, CardContent, CardFooter, CardHeader, CardTitle } from './ui/card'
 import { Button } from './ui/button'
 import { useNavigate } from 'react-router-dom'
 import { AppDispatch } from '@/redux/store'
-import { useAppDispatch } from '@/hooks'
-import { setSortingOption } from '@/redux/sortingSlice'
+import { useAppDispatch } from '@/lib/hooks'
+import { setCurrentPostId } from '@/redux/postPageSlice'
+import { setCurrentSubreddit } from '@/redux/subredditSlice'
 
 export const Feed = ({
     className,
@@ -14,9 +15,22 @@ export const Feed = ({
     className?: string
     postsArray: Array<any>
     }) => {
+    
+       const navigate = useNavigate()
+       const dispatch: AppDispatch = useAppDispatch()
+
+       const handlePostClick = (path: string) => {
+           navigate(`/posts/${path}`)
+           dispatch(setCurrentPostId(path))
+       }
+    
+    const handleSubredditClick = (path: string) => { 
+        navigate(`/r/${path}`)
+        dispatch(setCurrentSubreddit(path))
+    }
    
     return (
-        <div className={cn('flex flex-col py-2 px-11 h-full gap-4', className)}>
+        <div className={cn('flex flex-col py-2 px-3 h-full gap-4', className)}>
            
             {postsArray.map((post) => {
                 const hasImagePattern = /\.(jpeg|jpg|gif|png)$/
@@ -29,7 +43,7 @@ export const Feed = ({
                 return (
                     <Card
                         key={post.data.id}
-                        className={`bg-card grid  auto-rows-auto h-full content-center border-2 border-border rounded-xl shadow-md shadow-black/20 hover:bg-card-hover transition ${
+                        className={`bg-card grid  auto-rows-auto h-full content-center border-[1px] border-border rounded-xl shadow-md shadow-black/20 transition-all ${
                             urlHasImage || destHasImage || hasVideo
                                 ? 'grid-cols-1'
                                 : 'grid-cols-1'
@@ -37,13 +51,24 @@ export const Feed = ({
                     >
                         <CardHeader>
                             <div className="flex flex-row items-center justify-between text-xs">
-                                <p className="">/r/{post.data.subreddit}</p>
+                                <a
+                                    onClick={() =>
+                                        handleSubredditClick(
+                                            post.data.subreddit
+                                        )
+                                    }
+                                    className="hover:cursor-pointer"
+                                >
+                                    /r/{post.data.subreddit}
+                                </a>
                                 <p>{getRelativeTime(post.data.created)}</p>
                             </div>
                             <CardTitle>
                                 <a
-                                    href="/"
-                                    className="text-card-foreground text-xl font-bold hover:text-accent transition delay-150"
+                                    onClick={() =>
+                                        handlePostClick(post.data.id)
+                                    }
+                                    className="text-card-foreground text-3xl font-bold transition delay-150 hover:cursor-pointer"
                                 >
                                     {post.data.title}
                                 </a>
@@ -59,7 +84,7 @@ export const Feed = ({
                                               ? post.data.url_overridden_by_dest
                                               : undefined
                                     }
-                                    className={`col-span-3 row-span-2  object-cover w-full px-3 pb-3 rounded-[2rem]`}
+                                    className={`col-span-3 row-span-2  object-cover w-full px-3 pb-3 rounded-[2rem] max-h-[30rem]`}
                                     alt="thumbnail"
                                 />
                             ) : hasVideo ? (
@@ -69,7 +94,7 @@ export const Feed = ({
                                             'fallback_url'
                                         ]
                                     }
-                                    className="col-span-3 row-span-2 object-cover w-full px-3 pb-3 rounded-[2rem]"
+                                    className="col-span-3 row-span-2 object-cover w-full px-3 pb-3 rounded-[2rem] max-h-[30rem]"
                                     controls
                                 />
                             ) : null}
@@ -85,13 +110,13 @@ export const Feed = ({
                                 <div className="flex flex-row gap-3">
                                     <Button
                                         variant="secondary"
-                                        className="flex flex-row items-center"
+                                        className="flex flex-row items-center gap-1"
                                     >
                                         <ArrowBigUp /> {post.data.ups}
                                     </Button>
                                     <Button
                                         variant="secondary"
-                                        className="flex flex-row items-center"
+                                        className="flex flex-row items-center gap-1"
                                     >
                                         <MessageCircle />{' '}
                                         {post.data.num_comments}
