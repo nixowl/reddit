d54af8cc9936696bc3aead007786c0f7d5ce9451
nixowl <gabriellamondvay@gmail.com>
2024-04-15 12:37:49 +0200

    move similar fetching logic to one file

--- src/api/getPosts.ts
+++ src/api/getPosts.ts
@@ -4,3 +4,17 @@ export const getPosts = async () => {
     const response = await axios.get('https://www.reddit.com/hot.json?t=day?limit=5')
     return response.data
 }
+
+export const getTodaysHottest = async () => {
+    const response = await axios.get(
+        'https://www.reddit.com/top.json?t=day?limit=5'
+    )
+    const data = response.data.data.children
+    const firstFiveWithImages = data.filter(
+        (post: any) =>
+            post.data.url.includes('jpg') ||
+            post.data.url.includes('png') ||
+            post.data.url.includes('jpeg')
+    )
+    return firstFiveWithImages.slice(0, 5)
+}
--- src/api/getTodaysHottest.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-export const getTodaysHottest = async () => {
-    const response = await axios.get('https://www.reddit.com/top.json?t=day?limit=5')
-    const data = response.data.data.children
-    const firstFiveWithImages = data.filter((post: any) => post.data.url.includes('jpg') || post.data.url.includes('png') || post.data.url.includes('jpeg'))
-    return firstFiveWithImages.slice(0, 5)
-}
