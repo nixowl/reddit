3413e4fb2d23147fc59d93421c9d34bd6d8d4400
nixowl <gabriellamondvay@gmail.com>
2024-04-15 12:42:54 +0200

    add helper function to get relative time based on "created at" property

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,30 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+    return twMerge(clsx(inputs))
+}
+
+export function getRelativeTime(unixTimestamp: number) {
+    const currentTime = new Date().getTime() / 1000
+    const timeDifference = currentTime - unixTimestamp
+
+    const intervals = [
+        { label: 'year', seconds: 31536000 },
+        { label: 'month', seconds: 2592000 },
+        { label: 'day', seconds: 86400 },
+        { label: 'hour', seconds: 3600 },
+        { label: 'minute', seconds: 60 },
+        { label: 'second', seconds: 1 },
+    ]
+
+    for (const interval of intervals) {
+        const count = Math.floor(timeDifference / interval.seconds)
+        if (count >= 1) {
+            const plural = count === 1 ? '' : 's'
+            return `${count} ${interval.label}${plural} ago`
+        }
+    }
+
+    return 'Just now'
 }
