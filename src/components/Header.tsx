f105999cac238db2250e55bade75d0a7828649d2
nixowl <gabriellamondvay@gmail.com>
2024-04-15 12:39:05 +0200

    fix weird button placement

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import { Bird } from 'lucide-react'
 import { cn } from '@/lib/utils'
 import { useState } from 'react';
 import UserDropdown from './UserDropDown';
+import { Button } from './ui/button';
 
 type HeaderProps = {
     onMenuToggle: (isOpen: boolean) => void
@@ -11,25 +12,14 @@ const Header: React.FC<HeaderProps> = ({ className, onMenuToggle }: { className?
 
   const [isMenuOpen, setIsMenuOpen] = useState(false);
     return (
-        <header className={cn('w-full bg-background px-2 py-1', className)}>
-            <nav className="max-w-screen-xl flex flex-wrap items-center justify-between lg:justify-around mx-auto border-t-0 border-b-[1px] ">
-                <a
-                    href="/"
-                    className="flex items-center space-x-3 rtl:space-x-reverse"
-                >
-                    <Bird />
-                    <h1 className="text-2xl">scrowl</h1>
-                </a>
-
-                <div className="items-center justify-between" id="navbar-user">
-                    <input
-                        type="text"
-                        id="search-navbar"
-                        className="flex w-full rounded-xl border border-input bg-transparent p-3 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
-                        placeholder="Search..."
-                    />
-                </div>
-                <div className="flex items-center md:order-2 space-x-3 p-2 md:space-x-0 rtl:space-x-reverse">
+        <header
+            className={cn(
+                'fixed z-50 top-0 w-full border-b-[1px] bg-background backdrop-blur-sm px-2 py-1',
+                className
+            )}
+        >
+            <nav className="max-w-screen-xl flex flex-wrap items-center justify-between lg:justify-around mx-auto p-2">
+                <div className="flex items-center space-x-3 p-2 ">
                     <UserDropdown
                         isOpen={isMenuOpen}
                         onClose={() => setIsMenuOpen(false)}
@@ -38,12 +28,7 @@ const Header: React.FC<HeaderProps> = ({ className, onMenuToggle }: { className?
                     <button
                         data-collapse-toggle="navbar-user"
                         type="button"
-                        aria-expanded={isMenuOpen}
-                        onClick={() => setIsMenuOpen(!isMenuOpen)}
-                        data-dropdown-toggle="user-dropdown"
-                        data-dropdown-placement="bottom"
-                        id="user-menu-button"
-                        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                        className="md:hidden inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                         aria-controls="navbar-user"
                     >
                         <span className="sr-only">Open main menu</span>
@@ -64,6 +49,38 @@ const Header: React.FC<HeaderProps> = ({ className, onMenuToggle }: { className?
                         </svg>
                     </button>
                 </div>
+                <a
+                    href="/"
+                    className="flex items-center space-x-3 rtl:space-x-reverse"
+                >
+                    <Bird />
+                    <h1 className="text-2xl hidden md:block">scrowl</h1>
+                </a>
+
+                <div className="items-center justify-between" id="navbar-user">
+                    <input
+                        type="text"
+                        id="search-navbar"
+                        className="flex w-full rounded-full border border-input bg-background py-2 px-5 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                        placeholder="Search..."
+                    />
+                </div>
+                <Button
+                    variant="secondary" 
+                    className="px-5 py-1 text-md">
+                    Log In
+                </Button>
+                <Button
+                    variant="outline"
+                    size="icon"
+                    aria-expanded={isMenuOpen}
+                    onClick={() => setIsMenuOpen(!isMenuOpen)}
+                    data-dropdown-toggle="user-dropdown"
+                    data-dropdown-placement="bottom"
+                    id="user-menu-button"
+                >
+                    hi
+                </Button>
             </nav>
         </header>
     )
